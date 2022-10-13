d42a639c6c47df607510af38d3be504a6d594f33
CursoFlutterVictor <cursofluttervictor@gmail.com>
2022-10-13 13:29:06 +0200

    Model generated

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// GENERAR EL MODELO DESDE UN ARCHIVO JSON
+// get generate model on home with assets/models/user.json --skipProvider
+
+// GENERAR EL MODELO DESDE UNA URL
+// get generate model on home from "https://api.github.com/users/CpdnCristiano"
+
+//import 'package:api_rest/models/proof_model.dart';
+
+void main() {
+  //ProofModel p = ProofModel.fromJson();
+
+  print("Hola");
+}
